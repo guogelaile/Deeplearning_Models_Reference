cc630a1ba0f3a7e19f6b15c5a17a3857a755e0a7
Mr.Liao <32917112+guogelaile@users.noreply.github.com>
2021-05-19 08:54:15 +0800

    Update pascalvoc_to_tfrecords.py

--- SSD-Tensorflow-master/datasets/pascalvoc_to_tfrecords.py
+++ SSD-Tensorflow-master/datasets/pascalvoc_to_tfrecords.py
@@ -80,7 +80,7 @@ def _process_image(directory, name):
     """
     # Read the image file.
     filename = directory + DIRECTORY_IMAGES + name + '.jpg'
-    image_data = tf.gfile.FastGFile(filename, 'r').read()
+    image_data = tf.gfile.FastGFile(filename, 'rb').read()  #将'r'改为'rb'-ljg-2021.5.19
 
     # Read the XML annotation file.
     filename = os.path.join(directory, DIRECTORY_ANNOTATIONS, name + '.xml')
