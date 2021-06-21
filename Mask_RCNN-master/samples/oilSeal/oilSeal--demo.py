1e87b8f322f66158577d8e85ab4869391b90d926
Mr.Liao <32917112+guogelaile@users.noreply.github.com>
2021-06-21 19:11:53 +0800

    divide train and test

--- Mask_RCNN-master/samples/oilSeal/oilSeal--demo.py
+++ Mask_RCNN-master/samples/oilSeal/oilSeal--demo.py
@@ -33,6 +33,10 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
+from mrcnn import visualize
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -123,15 +127,15 @@ class BalloonDataset(utils.Dataset):
             # the outline of each object instance. These are stores in the
             # shape_attributes (see json format above)
             # The if condition is needed to support VIA versions 1.x and 2.x.
-            # if type(a['regions']) is dict:
-            #     polygons = [r['shape_attributes'] for r in a['regions'].values()]
-            # else:
-            #     polygons = [r['shape_attributes'] for r in a['regions']]
-            polygons = [r['shape_attributes'] for r in a['regions']]
-            name = [r['region_attributes']['oilseal'] for r in a['regions']]
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']]
+            #polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            # name = [r['region_attributes']['name'] for r in a['regions'].values()]
             # # 序列字典
-            name_dict = {"canliao": 1, "liekai": 2, "yiwu": 3}
-            name_id = [name_dict[a] for a in name]
+            # name_dict = {"canliao": 1, "liekai": 2, "yiwu": 3}
+            # name_id = [name_dict[a] for a in name]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
@@ -144,7 +148,6 @@ class BalloonDataset(utils.Dataset):
                 "oilSeal",
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
-                class_id=name_id,
                 width=width, height=height,
                 polygons=polygons)
 
@@ -159,13 +162,12 @@ class BalloonDataset(utils.Dataset):
         image_info = self.image_info[image_id]
         if image_info["source"] != "oilSeal":
             return super(self.__class__, self).load_mask(image_id)
+
         # Convert polygons to a bitmap mask of shape
         # [height, width, instance_count]
-        name_id = image_info["class_id"]
         info = self.image_info[image_id]
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
                         dtype=np.uint8)
-        class_ids = np.array(name_id, dtype=np.int32)
         for i, p in enumerate(info["polygons"]):
             # Get indexes of pixels inside the polygon and set them to 1
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
@@ -173,7 +175,7 @@ class BalloonDataset(utils.Dataset):
 
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
-        return (mask.astype(np.bool), class_ids)
+        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype=np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -183,28 +185,6 @@ class BalloonDataset(utils.Dataset):
         else:
             super(self.__class__, self).image_reference(image_id)
 
-
-def train(model):
-    """Train the model."""
-    # Training dataset.
-    dataset_train = BalloonDataset()
-    dataset_train.load_balloon(args.dataset, "train")
-    dataset_train.prepare()
-
-    # Validation dataset
-    dataset_val = BalloonDataset()
-    dataset_val.load_balloon(args.dataset, "val")
-    dataset_val.prepare()
-
-    # *** This training schedule is an example. Update to your needs ***
-    # Since we're using a very small dataset, and starting from
-    # COCO trained weights, we don't need to train too long. Also,
-    # no need to train all layers, just the heads should do it.
-    print("Training network heads")
-    model.train(dataset_train, dataset_val,
-                learning_rate=config.LEARNING_RATE,
-                epochs=30,
-                layers='heads')
 
 
 def color_splash(image, mask):
@@ -216,7 +196,7 @@ def color_splash(image, mask):
     """
     # Make a grayscale copy of the image. The grayscale copy still
     # has 3 RGB channels, though.
-    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 0
     # Copy color pixels from the original color image where mask is set
     if mask.shape[-1] > 0:
         # We're treating all instances as one, so collapse the mask into one layer
@@ -230,149 +210,48 @@ def color_splash(image, mask):
 def detect_and_color_splash(model, image_path=None, video_path=None):
     assert image_path or video_path
 
-    # Image or video?
-    if image_path:
-        # Run model detection and generate the color splash effect
-        print("Running on {}".format(args.image))
-        # Read image
-        image = skimage.io.imread(args.image)
-        # Detect objects
-        r = model.detect([image], verbose=1)[0]
-        # Color splash
-        splash = color_splash(image, r['masks'])
-        # Save output
-        file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-        skimage.io.imsave(file_name, splash)
-    elif video_path:
-        import cv2
-        # Video capture
-        vcapture = cv2.VideoCapture(video_path)
-        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = vcapture.get(cv2.CAP_PROP_FPS)
+    # Run model detection and generate the color splash effect
+    print("Running on {}".format(image_path))
+    # Read image
+    image = skimage.io.imread(image_path)
+    # Detect objects
+    r = model.detect([image], verbose=1)[0]
+    print(r['class_ids'])
+    visualize.display_instances(
+        image, r['rois'], r['masks'], r['class_ids'],
+        ["background","canliao", "liekai", "yiwu"], r['scores'],
+        show_bbox=False, show_mask=False, title="Predictions")
 
-        # Define codec and create video writer
-        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        vwriter = cv2.VideoWriter(file_name,
-                                  cv2.VideoWriter_fourcc(*'MJPG'),
-                                  fps, (width, height))
-
-        count = 0
-        success = True
-        while success:
-            print("frame: ", count)
-            # Read next image
-            success, image = vcapture.read()
-            if success:
-                # OpenCV returns images as BGR, convert to RGB
-                image = image[..., ::-1]
-                # Detect objects
-                r = model.detect([image], verbose=0)[0]
-                # Color splash
-                splash = color_splash(image, r['masks'])
-                # RGB -> BGR to save image to video
-                splash = splash[..., ::-1]
-                # Add image to video writer
-                vwriter.write(splash)
-                count += 1
-        vwriter.release()
-    print("Saved to ", file_name)
+    plt.savefig("123.png")
+    #plt.show()
+    # Color splash
+    # splash = color_splash(image, r['masks'])
+    # # Save output
+    # file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+    # skimage.io.imsave(file_name, splash)
+    # print("Saved to ", file_name)
 
 
 ############################################################
-#  Training
+#  inference
 ############################################################
 
 if __name__ == '__main__':
-    import argparse
-
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect balloons.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="'train' or 'splash'")
-    parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/balloon/dataset/",
-                        help='Directory of the Balloon dataset')
-    parser.add_argument('--weights', required=True,
-                        metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
-                        help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
-    parser.add_argument('--video', required=False,
-                        metavar="path or URL to video",
-                        help='Video to apply the color splash effect on')
-    args = parser.parse_args()
-
-    # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video,\
-               "Provide --image or --video to apply color splash"
-
-    print("Weights: ", args.weights)
-    print("Dataset: ", args.dataset)
-    print("Logs: ", args.logs)
-
     # Configurations
-    if args.command == "train":
-        config = BalloonConfig()
-    else:
-        class InferenceConfig(BalloonConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-        config = InferenceConfig()
+    class InferenceConfig(BalloonConfig):
+        # Set batch size to 1 since we'll be running inference on
+        # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
+    config = InferenceConfig()
     config.display()
 
-    # Create model
-    if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
-    else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
-
-    # Select weights file to load
-    if args.weights.lower() == "coco":
-        weights_path = COCO_WEIGHTS_PATH
-        # Download weights file
-        if not os.path.exists(weights_path):
-            utils.download_trained_weights(weights_path)
-    elif args.weights.lower() == "last":
-        # Find last trained weights
-        weights_path = model.find_last()
-    elif args.weights.lower() == "imagenet":
-        # Start from ImageNet trained weights
-        weights_path = model.get_imagenet_weights()
-    else:
-        weights_path = args.weights
+    model = modellib.MaskRCNN(mode="inference", config=config, model_dir="logs")
 
     # Load weights
-    print("Loading weights ", weights_path)
-    if args.weights.lower() == "coco":
-        # Exclude the last layers because they require a matching
-        # number of classes
-        model.load_weights(weights_path, by_name=True, exclude=[
-            "mrcnn_class_logits", "mrcnn_bbox_fc",
-            "mrcnn_bbox", "mrcnn_mask"])
-    else:
-        model.load_weights(weights_path, by_name=True)
+    weights_path = '../../mask_rcnn_oilseal_0029.h5'
+    model.load_weights(weights_path, by_name=True)
 
     # Train or evaluate
-    if args.command == "train":
-        train(model)
-    elif args.command == "splash":
-        detect_and_color_splash(model, image_path=args.image,
-                                video_path=args.video)
-    else:
-        print("'{}' is not recognized. "
-              "Use 'train' or 'splash'".format(args.command))
+    detect_and_color_splash(model, image_path="../../datasets/oilSeal/val/liekai-1.bmp")
+    #detect_and_color_splash(model, image_path="D:/03-Cases/01-佳盼(公司名) 油封（产品）/04-打光测试\不良产品图\环光+背光（普通镜头）/沟槽残料/沟槽残料.bmp")
